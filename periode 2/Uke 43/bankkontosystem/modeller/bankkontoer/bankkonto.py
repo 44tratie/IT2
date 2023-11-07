d49145f590e7a1220df94f2aff66cb4900391702
Tien <44tratie@stud.akademiet.no>
2023-11-07 13:20:04 +0100

    legg til csv mulighet

--- periode 2/Uke 43/bankkontosystem/modeller/bankkontoer/bankkonto.py
+++ periode 2/Uke 43/bankkontosystem/modeller/bankkontoer/bankkonto.py
@@ -1,3 +1,5 @@
+from typing import Self
+
 from modeller.person import Person
 
 
@@ -5,14 +7,29 @@ class Bankkonto:
     def __init__(self, eier: Person, kontonummer: str, start_saldo: float = 0) -> None:
         self.eier = eier
         self.kontonummer = kontonummer
-        self.saldo = start_saldo
+        self._saldo = start_saldo
+
+    @property
+    def saldo(self) -> float:
+        return self._saldo
+
+    @classmethod
+    def fra_csv(cls, filbane: str, *args, **kwargs) -> Self:
+        with open(filbane) as csv_fil:
+            full_navn, landskode, tlfnummer, kontonummer = csv_fil.readline().split(",")
+
+        navn = full_navn.split()
+        fornavn = " ".join(navn[:-1])
+        etternavn = navn[-1]
+        eier = Person(fornavn, etternavn, tlfnummer, landskode)
+        return cls(eier, kontonummer, *args, **kwargs)
 
     def innskudd(self, beløp: float) -> bool:
         if beløp <= 0:
             print("Du kan bare ha innskudd på mer enn 0 kr!")
             return False
 
-        self.saldo += beløp
+        self._saldo += beløp
         print(f"Du satte inn {beløp:.2f} kr")
         self._vis_ny_saldo()
         return True
@@ -22,7 +39,7 @@ class Bankkonto:
             self._vis_saldo()
             return False
 
-        self.saldo -= beløp
+        self._saldo -= beløp
         print(f"Du tok ut {beløp:.2f} kr")
         self._vis_ny_saldo()
         return True
