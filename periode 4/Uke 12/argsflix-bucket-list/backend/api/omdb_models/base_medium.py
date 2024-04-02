e29dc3c71adb2659bfb286ea848cd119047cbdbf
Tien <44tratie@stud.akademiet.no>
2024-04-02 09:22:29 +0200

    fix missing poster

--- periode 4/Uke 12/argsflix-bucket-list/backend/api/omdb_models/base_medium.py
+++ periode 4/Uke 12/argsflix-bucket-list/backend/api/omdb_models/base_medium.py
@@ -1,6 +1,8 @@
 from typing import Literal
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
+
+from .missing_poster import BLANK_FILE_SVG
 
 
 class BaseMedium(BaseModel):
@@ -9,3 +11,8 @@ class BaseMedium(BaseModel):
     imdb_id: str = Field(alias="imdbID")
     type_: Literal["movie", "series", "game"] = Field(alias="Type")
     poster: str = Field(alias="Poster")
+
+    @field_validator("poster", mode="before")
+    @classmethod
+    def validate_url(cls, raw: str) -> str:
+        return raw if raw != "N/A" else BLANK_FILE_SVG
