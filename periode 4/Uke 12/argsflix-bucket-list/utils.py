2ec191f87110e0baa15389585d301e48ecc7574f
Tien <44tratie@stud.akademiet.no>
2024-04-02 10:55:07 +0200

    typehint multisort

--- periode 4/Uke 12/argsflix-bucket-list/utils.py
+++ periode 4/Uke 12/argsflix-bucket-list/utils.py
@@ -1,8 +1,10 @@
 from operator import attrgetter
-from typing import Iterable
+from typing import Iterable, TypeVar
+
+T = TypeVar("T")
 
 
-def multisort(iterable: Iterable, sort_specs: list[tuple[str, bool]]):
+def multisort(iterable: Iterable[T], sort_specs: list[tuple[str, bool]]) -> list[T]:
     for sort_by, reverse in reversed(sort_specs):
         iterable = sorted(iterable, key=attrgetter(sort_by), reverse=reverse)
     return iterable
